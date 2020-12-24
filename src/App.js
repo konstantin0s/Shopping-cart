a87b8ecaabba098db091b6185336854ad58773f7
konstantin0s <constantintofan85@gmail.com>
2020-12-24 21:48:51 +0100

    Add header and footer + css style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="grid-container">
+     <header>
+       <a href="/">Wishing list</a>
+     </header>
+     <main>Product List</main>
+
+     <footer>All rights reserved.</footer>
 
     </div>
   );
