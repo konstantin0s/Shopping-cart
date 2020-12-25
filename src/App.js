53bedc35f199ac82397de21406d3fa314fd66c80
konstantin0s <constantintofan85@gmail.com>
2020-12-25 12:56:31 +0100

    Add fake data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 
 import './App.css';
+import data from './data.json';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="grid-container">
-     <header>
-       <a href="/">Wishing list</a>
-     </header>
-     <main>Product List</main>
-
-     <footer>All rights reserved.</footer>
-
-    </div>
-  );
+export default class App extends Component {
+constructor() {
+  super();
+  this.state = {
+    products: data.products,
+    size: '',
+    sort: ''
+  }
 }
 
-export default App;
+  render() {
+
+      return (
+        <div className="grid-container">
+         <header>
+           <a href="/">Wishing list</a>
+         </header>
+         <main>Product List</main>
+
+         <div classsName="content">
+         <div classsName="main-content">
+         Products
+           </div>
+           <div classsName="sidebar">
+           Cart Items
+           </div>
+         </div>
+    
+         <footer>All rights reserved.</footer>
+    
+        </div>
+      );
+
+  }
+}
+
